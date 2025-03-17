82c143ba7569dc98565473324ae9255619c1d416
mar.torras <mar.torras@estudiantat.upc.edu>
2025-03-17 17:54:25 +0100

    ShowAllIssues basico implementado

--- homepage/models.py
+++ homepage/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Issue(models.Model):
     subject = models.CharField(max_length=30)
     description = models.CharField(max_length=280)
-    #status
-    #type
-    #serverities
-    #priority
+    status = models.CharField(max_length=20,default='New')
+    type = models.CharField(max_length=20,default='Question')
+    severity =models.CharField(max_length=20,default='Minor')
+    priority = models.CharField(max_length=20,default='Normal')
--- homepage/views.py
+++ homepage/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 from .models import Issue
 
 def showAllIssues(request):
-    issues = Issue.objects.all()
+    issues = Issue.objects.all().order_by('-id')
     return render(request, "showAllIssues.html", {'issues': issues})
