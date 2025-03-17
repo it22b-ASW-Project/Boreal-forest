404de414baddb2ac1914a9ee59e873af65682d69
Héctor Suárez <hector.suarez@estudiantat.upc.edu>
2025-03-17 17:20:58 +0100

    BD + ShowAllIssues sin orden, solo subject y description

--- boreal_forest/settings.py
+++ boreal_forest/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homepage'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'boreal_forest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'boreal_forest.sqlite3',
     }
 }
 
--- boreal_forest/urls.py
+++ boreal_forest/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 
-
 urlpatterns = [
-    path("hello/",include("hello.urls")),
+    path('', include("homepage.urls")),
     path('admin/', admin.site.urls),
 ]
