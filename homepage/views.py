43d2ec3fd7aa11047626eb2ac01efb344f70f93f
arnau.farran <arnau.farran@estudiantat.upc.edu>
2025-03-19 23:54:09 +0100

    Add new Issue

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 from .models import Issue
 
 def showAllIssues(request):
     issues = Issue.objects.all().order_by('-id')
     return render(request, "showAllIssues.html", {'issues': issues})
+def createIssue(request):
+    if request.method == 'POST':
+        # Procesa los datos del formulario aquí
+        subject = request.POST.get('Subject')
+        description = request.POST.get('description')
+        # Guarda el nuevo issue en la base de datos
+        new_issue = Issue(subject=subject, description=description)
+        new_issue.save()
+        # ...
+        return redirect('/')  # Redirige a la página principal después de crear el issue
+    return render(request, 'createIssue.html')
