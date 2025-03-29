2949794198a42ebfa4318adfbac3d2b083db9803
mar.torras <mar.torras@estudiantat.upc.edu>
2025-03-29 14:12:35 +0100

    filtre per parametres ja quan selecciones

--- homepage/filters.py
+++ homepage/filters.py
@@ -1,23 +1,43 @@
 from django.db.models import Q
 import django_filters
+from django import forms
 from .models import Issue
 
 from .models import Priority, Type, Severity, Status
 
 class IssueFilter(django_filters.FilterSet):
-    q = django_filters.CharFilter(method='filter_search', label="Buscar")
+    q = django_filters.CharFilter(
+        method='filter_search', 
+        label="Buscar", 
+        widget=forms.TextInput(attrs={
+            "placeholder": "Subject or description", 
+            "onchange": "this.form.submit();"
+            })
+    )
     
     priority = django_filters.ModelChoiceFilter(
-        queryset=Priority.objects.all(), label="Prioridad", empty_label="Todas"
+        queryset=Priority.objects.all(), 
+        label="Prioridad", 
+        empty_label="Todas",
+        widget=forms.Select(attrs={"onchange": "this.form.submit();"})
     )
     type = django_filters.ModelChoiceFilter(
-        queryset=Type.objects.all(), label="Tipo", empty_label="Todos"
+        queryset=Type.objects.all(), 
+        label="Tipo", 
+        empty_label="Todos",
+        widget=forms.Select(attrs={"onchange": "this.form.submit();"})
     )
     severity = django_filters.ModelChoiceFilter(
-        queryset=Severity.objects.all(), label="Severidad", empty_label="Todas"
+        queryset=Severity.objects.all(), 
+        label="Severidad", 
+        empty_label="Todas",
+        widget=forms.Select(attrs={"onchange": "this.form.submit();"})
     )
     status = django_filters.ModelChoiceFilter(
-        queryset=Status.objects.all(), label="Estado", empty_label="Todos"
+        queryset=Status.objects.all(), 
+        label="Estado", 
+        empty_label="Todos",
+        widget=forms.Select(attrs={"onchange": "this.form.submit();"})
     )
 
 
