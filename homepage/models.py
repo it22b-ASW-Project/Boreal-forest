4a8126acda192d85743dd5de0bab2926fc9cf734
Héctor Suárez <hector.suarez@estudiantat.upc.edu>
2025-04-04 17:03:13 +0200

    Assing to me i Watch

--- homepage/models.py
+++ homepage/models.py
@@ -37,7 +37,6 @@ class Issue(models.Model):
     priority = models.ForeignKey(Priority, on_delete=models.SET_NULL, null=True, blank=True)
     deadline = models.DateField(null=True, blank=True)
     created_by = models.ForeignKey('socialaccount.socialaccount', on_delete=models.SET_NULL, null=True, blank=True, related_name='creator')
-    assigned = models.ForeignKey('socialaccount.socialaccount', on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_to')
 
     def get_priority_color(self):
             return self.priority.color if self.priority else "#808080"  # Gris por defecto
@@ -54,3 +53,7 @@ class Issue(models.Model):
 class Watch(models.Model):
         watcher = models.ForeignKey('socialaccount.socialaccount', on_delete=models.SET_NULL, null=True, blank=True, related_name='watcher')
         issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+
+class Assigned(models.Model):
+    assigned = models.ForeignKey('socialaccount.socialaccount', on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned')
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
