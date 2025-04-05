dcc57dbe72573e50d07bc09fecedb9cf6a81ac6b
arnau.farran <arnau.farran@estudiantat.upc.edu>
2025-04-05 23:29:24 +0200

    Add file attachments

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include,path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('issues/new/', views.createIssue),
     path('issue/<int:id>/', views.issueDetail, name='issueDetail')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
