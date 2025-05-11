52bd2e481eeea6bfa6f65ad61a3280298de333d6
Héctor Suárez <hector.suarez@estudiantat.upc.edu>
2025-05-11 15:44:13 +0200

    task #159: edit priority

--- homepage/serializers.py
+++ homepage/serializers.py
@@ -10,10 +10,22 @@ class IssueSerializer(serializers.ModelSerializer):
 class PrioritySerializer(serializers.ModelSerializer):
     class Meta:
         model = Priority
-        fields = ['name', 'color']
-        read_only_fields = ['position']
+        fields = '__all__'
+        #fields = ['name', 'color']
+        #read_only_fields = ['position']
 
-        def validate_name(self, value):
-            if not value.strip():
-                raise serializers.ValidationError("El nombre no puede estar vacío.")
-            return value    
+    def update(self, instance, validated_data):
+        new_name = validated_data.get('name', instance.name)
+        if new_name != instance.name:
+            # Borrar el viejo e insertar uno nuevo
+            instance.delete()
+            instance.name = new_name
+
+        instance.color = validated_data.get('color', instance.color)
+        instance.save()
+        return instance    
+
+    def validate_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("El nombre no puede estar vacío.")
+        return value
