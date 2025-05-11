0105410f0a0c6f3dbd4b3f4f5a7b2ee1724bc1ba
Héctor Suárez <hector.suarez@estudiantat.upc.edu>
2025-05-11 18:33:17 +0200

    task #175: get all statuses

--- homepage/serializers.py
+++ homepage/serializers.py
@@ -50,3 +50,25 @@ class TypeSerializer(serializers.ModelSerializer):
         if not value.strip():
             raise serializers.ValidationError("El nombre no puede estar vacío.")
         return value
+    
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Status
+        fields = ['name', 'color']
+        read_only_fields = ['position']
+
+    def update(self, instance, validated_data):
+        new_name = validated_data.get('name', instance.name)
+        if new_name != instance.name:
+            # Borrar el viejo e insertar uno nuevo
+            instance.delete()
+            instance.name = new_name
+
+        instance.color = validated_data.get('color', instance.color)
+        instance.save()
+        return instance    
+
+    def validate_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("El nombre no puede estar vacío.")
+        return value
